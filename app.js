a29e5a855796a0eb1a1a91431bf0d04f204a9915
danielaconceicao <daniela.3597@gmail.com>
2024-10-12 17:25:39 +0200

    organizing the code

--- app.js
+++ app.js
@@ -1,66 +1,33 @@
-//Visualizzare in pagina 5 numeri casuali.
-const spaceCasualNumbers = document.querySelector('.number');
-const numbers = [];
-let spaceTime = document.querySelector('.space-time');
-const btn = document.querySelector('.btn-timer');
+const spaceCasualNumbers = document.querySelector('.display-random-number');
 const spaceForm = document.querySelector('.space-form');
-const formEl = document.querySelector('.form');
+const formEl = document.querySelector('form');
+const randNumbers = [];
 
 
-/**
- *@param {number} max 
- *@param {number} min 
- *  */
+//Visualizzare in pagina 5 numeri casuali.
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
-
-/**
- *@param {number} max 
- *@param {number} min 
- *  */
-function getRandomNumber(min, max) {
+function gerateRandNumbers(min, max) {
     for (let i = 0; i < 5; i++) {
-        numbers.push(getRndInteger(min, max))
+        randNumbers.push(getRndInteger(min, max));
     }
-    return numbers
+    console.log(randNumbers);
+    return randNumbers
 }
 
-/** 
- * @param {Array} arr 
- */
-function genereteNumbersMarkup(arr) {
-    `<span>${arr[0]}</span>
-     <span>${arr[1]}</span>
-     <span>${arr[2]}</span>
-     <span>${arr[3]}</span>
-     <span>${arr[4]}</span>`
-}
+spaceCasualNumbers.insertAdjacentHTML('beforeend', gerateRandNumbers);
+spaceCasualNumbers.innerHTML = gerateRandNumbers(1, 100);
 
-let generatedNumber = genereteNumbersMarkup(numbers);
-
-spaceCasualNumbers.insertAdjacentHTML('beforeend', generatedNumber);
-spaceCasualNumbers.innerHTML = getRandomNumber(1, 100);
 
 //Da lì parte un timer di 30 secondi.
-btn.addEventListener('click', () => {
-    let seconds = '00:00';
-    let clockCounter = 0;
-    let getTimer = setInterval(() => {
-        seconds = '00:' + clockCounter.toString().padStart(2, '0');
-        console.log(seconds)
-        spaceTime.textContent = seconds;
-        ++clockCounter;
-        if (clockCounter > 2 + 1) {
-            clearInterval(getTimer);
-            hideNumber();
-            spaceTime.textContent = "il tempo è scaduto, inserisci i numeri nell'input";
-            showHideForm();
-            btn.classList.toggle('d-none');
-        }
-    }, 1000)
-});
+setTimeout(() => {
+    document.querySelector('.display-random-number').innerHTML = '';
+    spaceForm.classList.remove('d-none');
+        
+}, 4000);
+
 
 //Dopo 30 secondi i numeri scompaiono e appaiono invece 5 input in cui l'utente deve inserire i numeri che ha visto precedentemente, nell'ordine che preferisce.
 formEl.addEventListener('submit', (e) => {
@@ -74,48 +41,15 @@ formEl.addEventListener('submit', (e) => {
     }
 
     const correctNumbers = [];
-    const wrongNumbers = [];
-
-    for (let i = 0; i < numbers.length; i++) {
-
-        if (userNumbers[i] === numbers[i]) {
+    for (let i = 0; i < randNumbers.length; i++) {
+        randN = randNumbers[i]
+        if (userNumbers[i] === randN ) {
             correctNumbers.push(userNumbers[i]);
-            
-        }else{
-            wrongNumbers.push(userNumbers[i]);
         } 
     }
-    showHideForm();
-    hideNumber();
 
-    //Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati
-    let number = correctNumbers.length <= 0 ? 'numero' : 'numeri';
-
-    if (correctNumbers) {
-        spaceTime.textContent = `Ti sei ricordato, ${correctNumbers.length} ${number} ${correctNumbers}`
-    }
-    //console.log(`numeros corretos`, correctNumbers);
-    //console.log(`numeros incorretos`, wrongNumbers);
+    spaceForm.classList.add('d-none');
+    return spaceCasualNumbers.innerHTML = `score:${correctNumbers.length} | You found: ${correctNumbers}`;
+    
+    
 });
-
-
-
-
-function hideNumber() {
-    if (spaceCasualNumbers){
-        document.querySelector('.number').innerHTML = '';
-    }
-}
-
-function showHideForm() {
-    if (spaceForm.classList.contains('d-none')) {
-        spaceForm.classList.replace('d-none', 'd-block');
-    }else {
-        spaceForm.classList.replace('d-block', 'd-none');
-    }
-}
-
-
-
-
-
